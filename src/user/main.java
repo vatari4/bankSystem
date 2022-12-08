41b62e9a0108bffb1c06a4f1bb2f59ef193458e6
VenomFeed <93274199+vatari4@users.noreply.github.com>
2022-12-08 23:31:47 +0700

    Create main.java

---
+++ src/user/main.java
@@ -0,0 +1,12 @@
+import programm.java.Programm;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Programm user = new Programm();
+
+        user.CreateUser();
+        user.Unit();
+        user.UserPrint();
+    }
+}
